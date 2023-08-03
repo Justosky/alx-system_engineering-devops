20ee04186442399cd599f8de2c747fe79458749e
justosky <justosky2011@yahoo.com>
2023-08-03 11:27:39 +0100

    A ruby script that uses regex to match patterns

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 # A ruby script that uses regex to match and extract portion of a log
 command = ARGV[0]
-regular_exp = /\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]/
+regular_exp = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
 puts command.scan(regular_exp).join
